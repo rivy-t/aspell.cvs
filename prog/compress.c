ffce3a99df28e94f519a3b93268d05d38ad353db
kevina <kevina>
2004-10-23 05:04:18 +0000

    Enhancements and bug fixed for compress.c by Jose Da Silva

--- prog/compress.c
+++ prog/compress.c
@@ -1,6 +1,9 @@
 /*
- * Copyright (c) 2000-2001
+ * Copyright (c) 2000-2004
  * Kevin Atkinson
+ * Jose Da Silva
+ *
+ * Word-list-compress Version 0.2.
  *
  * Permission to use, copy, modify, distribute and sell this software
  * and its documentation for any purpose is hereby granted without
@@ -10,6 +13,8 @@
  * representations about the suitability of this software for any
  * purpose.  It is provided "as is" without express or implied
  * warranty.
+ * 
+ * Bug fixes and enhancements by Jose Da Silva, 2004.
  *
  */
 
@@ -28,101 +33,113 @@
 
 #endif
 
-#define BUFSIZE 256
+#define BUFSIZE 256	/* BUFSIZE must be 256 */
 
-void usage () 
+static void usage ()
 {
-  fputs("Compresses or uncompresses sorted word lists.\n"     , stderr);
-  fputs("For best result the locale should be set to C\n"    , stderr);
-  fputs("before sorting by setting the environmental\n"       , stderr);
-  fputs("variable LANG to \"C\" before sorting.\n"            , stderr);
-  fputs("Copyright 2001,2004 by Kevin Atkinson.\n"  , stderr);
-  fputs("Usage: word-list-compress c[ompress]|d[ecompress]\n" , stderr);
+  fputs("Compresses or uncompresses sorted word lists.  Version 0.2.\n",        stderr);
+  fputs("For best result the locale should be set to C before sorting by\n",    stderr);
+  fputs("  setting the environmental variable LANG to \"C\" before sorting.\n", stderr);
+  fputs("Copyright 2000-2004 by Kevin Atkinson.\n",                             stderr);
+  fputs("Usage: word-list-compress c[ompress]|d[ecompress]|v[version]\n",       stderr);
 }
 
 // PRECOND: bufsize >= 2
-static int get_word(FILE * in, char * w, size_t bufsize) 
+static int get_word(FILE * in, char * w)
 {
-  int c;
+  int bufsize = BUFSIZE - 1;
+  register int c;
+
   while (c = getc(in), c != EOF && c <= 32);
-  if (c == EOF) return 0;
-  do {
-    *w++ = (char)(c);
-    --bufsize;
-  } while (c = getc(in), c != EOF && c > 32 && bufsize > 1);
+  if (c != EOF) {
+    do {
+      *w++ = (char)(c);
+    } while (c = getc(in), c > 32 && c != EOF && --bufsize);
+  }
   *w = '\0';
   ungetc(c, in);
-  if (c == EOF) return 0;
-  else return 1;
+  if (c == EOF) return 0; /* done */
+  if (bufsize)  return 1; /* normal return */
+  return 2;		  /* error, word larger than 255 chars */
 }
 
 int main (int argc, const char *argv[]) {
 
-  if (argc != 2) {
+  if (argc == 2) {
+    char c = argv[1][0];
+    if (c == '-') c = argv[1][1];
 
-    usage();
-    return 1;
-    
-  } else if (argv[1][0] == 'c') {
+    if (c == 'c') {
 
-    char s1[BUFSIZE];
-    char s2[BUFSIZE];
-    char * prev = s2;
-    char * cur = s1;
-    *prev = '\0';
+      char s1[BUFSIZE];
+      char s2[BUFSIZE];
+      char * prev = s2;
+      char * cur = s1;
+      *prev = '\0';
+      int errFlag;
 
-    SETBIN (stdout);
+      SETBIN (stdout);
 
-    while (get_word(stdin, cur, BUFSIZE)) {
-      int i = 0;
-      /* get the length of the prefix */
-      while (prev[i] != '\0' && cur[i] != '\0' && prev[i] == cur[i])
-        ++i;
-      if (i > 31) {
-        putc('\0', stdout);
+      while ((errFlag = get_word(stdin, cur)) == 1) {
+        int i = 0;
+        /* get the length of the prefix */
+	while (prev[i] != '\0' && prev[i] == cur[i])
+          ++i;
+        if (i > 31) {
+	  if (putc('\0', stdout) < 0) goto error_out_c;
+        }
+	if (putc(i+1, stdout) < 0) goto error_out_c;
+	if (fputs(cur+i, stdout) < 0) goto error_out_c;
+
+	/* swap prev and next */
+	{
+	  char * tmp = cur;
+	  cur = prev;
+	  prev = tmp;
+        }
       }
-      putc(i+1, stdout);
-      fputs(cur+i, stdout);
-      if (cur == s1) {
-        prev = s1; cur = s2;
-      } else {
-        prev = s2; cur = s1;
+      if (fflush(stdout) < 0) goto error_out_c;
+      if (errFlag) goto error_in_c;
+      return 0;
+    }
+
+    if (c == 'd') {
+    
+      char cur[BUFSIZE+1];
+      int i;
+      int c;
+
+      SETBIN (stdin);
+    
+      i = getc(stdin);
+      if (i != 1) goto error_in_d;
+      while (i != -1) {
+        if (i == 0)
+          i = getc(stdin);
+        --i;
+        if (i < 0) goto error_in_d;
+        while ((c = getc(stdin)) > 32 && i < BUFSIZE)
+          cur[i++] = (char)c;
+	if (i >= BUFSIZE) goto error_in_d;
+	cur[i] = '\n'; cur[++i] = '\0';
+	if (fputs(cur, stdout) < 0) goto error_out_d;
+        i = c;
       }
+      return 0;
+      
+    error_in_c:
+    error_in_d:
+      fputs("ERROR: Corrupt Input.\n", stderr);
+      return 2;
+
+    error_out_c:
+    error_out_d:
+      /* output space full or other output fault */
+      fputs("ERROR: Output Data Error.\n", stderr);
+      return 3;
     }
-    return 0;
-
-  } else if (argv[1][0] == 'd') {
-    
-    char cur[256];
-    int i;
-    int c;
-
-    SETBIN (stdin);
-
-    i = getc(stdin);
-    while (i != -1 ) {
-      if (i == 0)
-        i = getc(stdin);
-      --i;
-      if (i < 0) goto error;
-      while ((c = getc(stdin)) > 32 && i < BUFSIZE)
-        cur[i++] = (char)c;
-      if (i >= BUFSIZE) goto error;
-      cur[i] = '\0';
-      fputs(cur, stdout);
-      putc('\n', stdout);
-      i = c;
-    }
-    return 0;
-
-   error:
-    fputs("ERROR: Corrupt Input.\n", stderr);
-    return 2;
-
-  } else {
-
-    usage();
-    return 1;
-    
   }
+  
+  usage();
+  return 1;
 }
